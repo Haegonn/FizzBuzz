114bac0c73723dfb298e61d833f845f6243329d6
Haegonn <JsDalton11@gmail.com>
2015-05-22 22:15:29 -0400

    Completed program
    Completed version one (and likely the only one) of the software.

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -9,9 +9,37 @@ namespace FizzBuzz
 {
     class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
-            // Do something..
+            // Write out a standard welcome message.
+            Console.WriteLine("Welcome to Josh's FizzBuzz Generator v42!\n");
+
+            // Let's initialize the loop limit.
+            int loopStop = 100;
+
+            // Time to get the loop started!
+            for (int i = 1; i <= loopStop; i++)
+            {
+                if (i % 3 == 0 && i % 5 != 0)
+                {
+                    Console.Write("Fizz ");
+                }
+                else if (i % 3 != 0 && i % 5 == 0)
+                {
+                    Console.Write("Buzz ");
+                }
+                else if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.Write("FizzBuzz ");
+                }
+                else
+                {
+                    Console.Write("{0} ", i);
+                }
+            }
+
+            // Pause the console.
+            Console.ReadLine();
         }
     }
 }
